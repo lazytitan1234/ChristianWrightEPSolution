6df2d17455b7a68ae4c468ea77b6956aacca52ed
lazytitan1234 <gaminglazytitan@gmail.com>
2025-04-04 13:24:29 +0200

    added the code for poll PollDbContext and PollRepository

--- DataAccess/DataContext/PollDbContext.cs
+++ DataAccess/DataContext/PollDbContext.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+
 
 namespace DataAccess.DataContext
 {
-    public class PollDbContext
+    public class PollDbContext : DbContext
     {
+        public PollDbContext(DbContextOptions<PollDbContext> options) : base(options) { }
+
+        public DbSet<Poll> Polls { get; set; }
     }
 }
--- DataAccess/Repositories/PollRepository.cs
+++ DataAccess/Repositories/PollRepository.cs
@@ -1,12 +1,70 @@
-﻿using System;
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DataAccess.DataContext;
 
 namespace DataAccess.Repositories
 {
     public class PollRepository
     {
+        private readonly PollDbContext _context;
+
+        public PollRepository(PollDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreatePoll(string title, string option1Text, string option2Text, string option3Text,
+                               int option1VotesCount, int option2VotesCount, int option3VotesCount,
+                               DateTime dateCreated)
+        {
+            var poll = new Poll
+            {
+                Title = title,
+                Option1Text = option1Text,
+                Option2Text = option2Text,
+                Option3Text = option3Text,
+                Option1VotesCount = option1VotesCount,
+                Option2VotesCount = option2VotesCount,
+                Option3VotesCount = option3VotesCount,
+                DateCreated = dateCreated
+            };
+
+            _context.Polls.Add(poll);
+            _context.SaveChanges();
+        }
+
+
+        public IEnumerable<Poll> GetPolls()
+        {
+            return _context.Polls
+                           .OrderByDescending(p => p.DateCreated)
+                           .ToList();
+        }
+
+        public void Vote(int pollId, int optionNumber)
+        {
+            var poll = _context.Polls.FirstOrDefault(p => p.Id == pollId);
+
+            if (poll != null)
+            {
+                switch (optionNumber)
+                {
+                    case 1:
+                        poll.Option1VotesCount++;
+                        break;
+                    case 2:
+                        poll.Option2VotesCount++;
+                        break;
+                    case 3:
+                        poll.Option3VotesCount++;
+                        break;
+                }
+
+                _context.SaveChanges();
+            }
+        }
     }
 }
