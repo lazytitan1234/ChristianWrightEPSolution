aa83e5bf1d1a389f3f021264059b387933c522e0
lazytitan1234 <gaminglazytitan@gmail.com>
2025-04-04 16:37:24 +0200

    added the PollViewModels

---
+++ Presentation/Models/PollViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Presentation.Models
+{
+    public class PollViewModel
+    {
+        public string Title { get; set; }
+        public string Option1Text { get; set; }
+        public string Option2Text { get; set; }
+        public string Option3Text { get; set; }
+    }
+}
