5110d49e6049214b30ffd0fd13c9f7d5b159a131
lazytitan1234 <gaminglazytitan@gmail.com>
2025-04-06 14:00:46 +0200

    should help hosting work from git

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -5,91 +5,60 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.WebHost.UseUrls("http://localhost:5000");
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-//builder.Services.AddDbContext<PollDbContext>(options =>
-//    options.UseSqlServer(connectionString));
+// Configure database connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<PollDbContext>(options =>
     options.UseSqlServer(connectionString, sqlOptions =>
     {
-        sqlOptions.EnableRetryOnFailure();
+        sqlOptions.EnableRetryOnFailure(); // Good for Azure transient faults
     }));
+
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false;
 })
 .AddEntityFrameworkStores<PollDbContext>();
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//    .AddEntityFrameworkStores<PollDbContext>();
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddRazorPages();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddSession();
 
+// Dependency Injection setup
 builder.Services.AddScoped<PollRepository>();
 builder.Services.AddScoped<PollFileRepository>();
-
 builder.Services.AddScoped<IPollRepository, DynamicPollRepository>();
-
-
-//var repoSetting = builder.Configuration.GetValue<int>("RepositorySetting");
-
-//switch (repoSetting)
-//{
-//    case 1:
-//        builder.Services.AddScoped<IPollRepository, PollFileRepository>();
-//        break;
-//    case 2:
-//    default:
-//        builder.Services.AddScoped<IPollRepository, PollRepository>();
-//        break;
-//}
-
-//builder.Services.AddScoped<IPollRepository, PollRepository>();
-//builder.Services.AddScoped<IPollRepository, PollFileRepository>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+// Exception Handling (show full errors even in production for now)
+app.UseDeveloperExceptionPage(); 
+
+if (!app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    // Don't run migrations automatically in production
+    // When not in dev, use error handler and HSTS
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
+// Core middleware
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseSession();
-
 app.UseAuthentication();
 app.UseAuthorization();
 
+// Route mapping
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+// Simple route to test if the app is alive
+app.MapGet("/ping", () => "pong"); 
 
 app.Run();
